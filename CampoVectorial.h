f46262442eec3d89b0f9c23c4362dcab2235085d
Marina García Aguado <92505924+marinagarciaguado@users.noreply.github.com>
2024-04-15 11:34:24 +0200

    Add files via upload

---
+++ CampoVectorial.h
@@ -0,0 +1,17 @@
+#pragma once 
+#include <iostream>
+using namespace std;
+
+class CampoVectorial {
+    private: 
+    string nombre;
+
+    public:
+    CampoVectorial();
+    CampoVectorial(string nombre);
+    virtual float calcularIntensidad(float posicion) = 0;
+    virtual float* calcularDireccion(float posicion) = 0;
+    string getNombre();
+    static float dist(float* punto1, float* punto2);
+    string toString;
+}
