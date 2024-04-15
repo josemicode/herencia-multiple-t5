806796642785697c0249928dd02222e0792f8e2d
josemicode <jmiguelojedagutierrez@gmail.com>
2024-04-15 11:43:14 +0200

    Investigador.h modified v1.3

--- Investigador.h
+++ Investigador.h
@@ -1,8 +1,15 @@
 class Investigador{
 	private:
 		int orcid;
+		int anyoInicio;
+		string areaConocimiento;
 	protected:
 		string institucion;
 	public:
-		
+		Investigador(orcid, institucion, anyoInicio, areaConocimento);
+		virtual string getAreaInvestigacion() = 0;
+		int getAnyosExperiencia();
+		string toString();
+		Investigador(const &Investigador otro);
+		Investigador operator = (const &Investigador otro);
 };
