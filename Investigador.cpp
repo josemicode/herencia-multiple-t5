c0a83fe5f3381299f15f75f77aaab89e8444f7d3
josemicode <jmiguelojedagutierrez@gmail.com>
2024-04-17 20:40:37 +0200

    Added Investigador.cpp

---
+++ Investigador.cpp
@@ -0,0 +1,42 @@
+#include"Investigador.h"
+
+using namespace std;
+
+Investigador::Investigador(int orcid, string institucion, int anyoInicio, string areaConocimiento){
+	this->orcid = orcid;
+	this->institucion = institucion;
+	this->anyoInicio = anyoInicio;
+	this->areaConocimiento = areaConocimiento;
+}
+
+int Investigador::getOrcid(){
+	return this->orcid;
+}
+
+string Investigador::getInstitucion(){
+	return this->institucion;
+}
+
+int Investigador::getAnyosExperiencia(){
+	return (2024-(this->anyoInicio));
+}
+
+string Investigador::toString(){
+	string res = "\n- Codigo orcid: " + to_string(this->orcid) + "\n- Institucion: " + this->institucion
+	+ "\n- Anyo de Inicio: " + to_string(this->anyoInicio) + "\n- Area de Conocimiento: " + this->areaConocimiento;
+}
+
+Investigador::Investigador(const Investigador &otro){
+	this->orcid = otro.orcid;
+	this->areaConocimiento = otro.areaConocimiento;
+	this->institucion = otro.institucion;
+}
+
+Investigador& Investigador::operator = (const Investigador &otro){
+	this->areaConocimiento = otro.areaConocimiento;
+	this->institucion = otro.institucion;
+}
+
+int main(){
+	return 0;
+}
